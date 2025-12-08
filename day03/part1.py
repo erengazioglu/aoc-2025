a21ffb5ce6d7fb15e1a3c548fa42aad754bf360f
POMPY PRODUCTIONS <accounts@pompyproductions.com>
2025-12-08 16:38:03 +0100

    solve day 03 part 1

--- day03/part1.py
+++ day03/part1.py
@@ -4,5 +4,37 @@ Advent of Code 2025 - Day 3 Part 1
 
 from util import read_input
 
+def get_max_left(bank:str, verbose:bool=False) -> tuple[int, int]:
+    max = (0, 0) # i, val
+    for i, char in enumerate(bank[:-1]):
+        if int(char) > max[1]:
+            max = (i, int(char))
+    if verbose:
+        print(f"bank {bank} | max_left = {max}")
+    return max
+
+
+def get_max_right(bank:str, start:int, verbose:bool=False) -> tuple[int, int]:
+    max = (0, 0) # i, val
+
+    for i, char in enumerate(bank[start:]):
+        if int(char) > max[1]:
+            max = (i, int(char))
+    if verbose:
+        print(f"bank {bank} | max_right = {max}")
+    return max
+
+
+def get_largest_jolt(bank:str, verbose:bool=False):
+    max_left = get_max_left(bank)
+    max_right = get_max_right(bank, max_left[0] + 1)
+    if verbose:
+        print(f"bank {bank} | {max_left[1] * 10 + max_right[1]}")
+    return max_left[1] * 10 + max_right[1]
+
+
 def part1(data:str, verbose:bool=False):
-    return read_input(data)
+    sum = 0
+    for bank in read_input(data):
+        sum += get_largest_jolt(bank, verbose)
+    return sum
--- day03/part2.py
+++ day03/part2.py
@@ -4,5 +4,15 @@ Advent of Code 2025 - Day 3 Part 2
 
 from util import read_input
 
+def get_max(bank:str, verbose:bool=False) -> tuple[int, int]:
+    max = (0, 0) # i, val
+
+    for i, char in enumerate(bank):
+        if int(char) > max[1]:
+            max = (i, int(char))
+    if verbose:
+        print(f"bank {bank} | max = {max}")
+    return max
+
 def part2(data:str, verbose:bool=False) -> int:
     return read_input(data)
