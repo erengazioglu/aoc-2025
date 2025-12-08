3e688de77617bb18c6b21ad89cc2238182d2244a
POMPY PRODUCTIONS <accounts@pompyproductions.com>
2025-12-08 13:23:45 +0100

    Create template

---
+++ template/part1.py
@@ -0,0 +1,8 @@
+"""
+Advent of Code 2025 - Day # Part 1
+"""
+
+from util import read_input
+
+def part1(data, verbose:bool=False):
+    return read_input(data)
---
+++ template/part2.py
@@ -0,0 +1,8 @@
+"""
+Advent of Code 2025 - Day # Part 2
+"""
+
+from util import read_input
+
+def part2(data:str, verbose:bool=False) -> int:
+    return read_input(data)
